9f6852b6546f9ee708895bd198ec0a2328be4e4d
zura0z <zuramazmishvili@gmail.com>
2020-10-03 13:38:26 +0400

    Updates

---
+++ precache-manifest.c38867f46e592e14226d3c0b69fd47ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b154cca7ceffa5e0081116f1633fa17",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "c60d5af01e87e2201d64",
+    "url": "/drum-machine/static/css/main.26956798.chunk.css"
+  },
+  {
+    "revision": "aacf56677e47617692b2",
+    "url": "/drum-machine/static/js/2.ccb0e14a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.ccb0e14a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c60d5af01e87e2201d64",
+    "url": "/drum-machine/static/js/main.d81cddd0.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
